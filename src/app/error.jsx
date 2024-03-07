973054a682e2865305ef4abe5ed62de45c5fe6fe
farushko <martinmilev@gmail.com>
2024-03-07 14:33:05 +0200

    Error

---
+++ src/app/error.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { useEffect } from "react"
+
+export default function error(error, reset) {
+    useEffect(() =>{
+        console.log(error);
+    }, [error]);
+  return (
+    <div className="text-center m-10">
+        <h1>Something went wrong. Please, try again!</h1>
+        <button className="hover:text-amber-600" onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
