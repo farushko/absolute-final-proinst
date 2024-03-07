dad1d6e4480ca09e36d4895459d16b45196574a9
farushko <martinmilev@gmail.com>
2024-03-07 11:18:45 +0200

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Navbaritem from "./Navbaritem";
+
+export default function Navbar() {
+  return (
+    <div className="flex dark:bg-gray-600 bg-amber-100 p-4 gap-3 justify-center lg:text-lg">
+      <Navbaritem title="Tranding" param="fetchTrending" />
+      <Navbaritem />
+      <Navbaritem title="Top Rated" param="fetchTopRated" />
+      <Navbaritem />
+    </div>
+  );
+}
