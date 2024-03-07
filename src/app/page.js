8c171e3d1e991c08ffab04404ab994f4205154f9
farushko <martinmilev@gmail.com>
2024-03-07 14:22:33 +0200

    Fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,22 @@
+const API_KEY = process.env.API_KEY;
+import Results from "@/components/Results";
 
-export default function Home() {
-  return (
-    <div>Home</div>
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=243f21f430b73f8baab38d64edc14e98&language=en-US&page=1`
   );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+  console.log(API_KEY);
 }
