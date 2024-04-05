7b68f02041ff445c4b561ee421a9cec793536cdd
farushko <martinmilev@gmail.com>
2024-04-05 15:26:28 +0200

    Menu

--- src/components/Menuitem.jsx
+++ src/components/Menuitem.jsx
@@ -20,7 +20,7 @@ export default function Menuitem() {
   const currentPath = usePathname();
   return (
     <div>
-      <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-stone-800 fixed nav top-0 left-0 md:relative">
+      <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-stone-800 fixed nav top-0 left-0 md:relative z-30">
         <ul className="hidden md:flex flex-col">
           {menuItems.map(({ id, address, title }) => (
             <li
@@ -43,13 +43,13 @@ export default function Menuitem() {
 
         <div
           onClick={() => setNav(!nav)}
-          className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+          className="cursor-pointer pr-4 z-50 text-gray-500 md:hidden"
         >
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
 
         {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 z-40">
             {menuItems.map(({ id, address, title }) => (
               <li
                 key={id}
